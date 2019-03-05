9473084fa796b38cd6dc40b973330d738db4b1bf
Rhiannan Berry <rhiannanb97@gmail.com>
2019-03-05 11:39:44 -0500

    Fixing animations and rigs and added a cactus :cactus:

---
+++ Assets/Scripts/AnimationManagement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManagement : MonoBehaviour
+{
+	public GameObject cowboyYellow, cowboyOrange;
+	public Transform ridingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRiding();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void SetRiding() {
+	cowboyYellow.transform.SetParent(ridingPosition);	
+	cowboyYellow.transform.position = ridingPosition.position;
+	cowboyYellow.transform.rotation = ridingPosition.rotation;
+	}
+}
